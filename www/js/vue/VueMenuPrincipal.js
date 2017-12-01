f8e3069d07ebc1a3d7f243660c5389f3367c34c2
BENETTI Mathilde <benettim@hostname.iut2.upmf-grenoble.fr>
2017-12-01 16:42:03 +0100

    MenuPrin

--- www/js/vue/VueMenuPrincipal.js
+++ www/js/vue/VueMenuPrincipal.js
@@ -4,18 +4,11 @@ class VueMenuPrincipal extends Vue {
 
 
     //Button option
-<<<<<<< HEAD
-    const buttonOptions = this.create('button');
-    this.add(buttonOptions);
-    buttonOptions.innerHTML = "Options";
-    buttonOptions.onclick = () => {
-=======
     const buttonOption = this.create('button');
     buttonOption.id="btnO";
     this.add(buttonOption);
     buttonOption.innerHTML = "Options";
     buttonOption.onclick = () => {
->>>>>>> 8a257cb7aaca9930ebbc4bee179566d48c40c09c
     this.controleur.changerVue(new VueOptions(this.controleur), this);
     }
     //titre

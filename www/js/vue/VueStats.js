876b986a35438c6efe85b09544ec9974ed03440b
MARTIN Jeremy <martinje@hostname.iut2.upmf-grenoble.fr>
2017-12-01 15:47:52 +0100

    Version_Jérémy03

--- www/js/vue/VueStats.js
+++ www/js/vue/VueStats.js
@@ -1,12 +1,11 @@
 class VueStats extends Vue {
   constructor(ctrl) {
     super();
-  }
 
-  const buttonRetour = this.create('button');
-  this.add(buttonRetour);
-  buttonRetour.innerHTML = "Menu Principal";
-  buttonRetour.onclick = () => {
-    this.controleur.changerVue(new VueMenuPrincipal(this.controleur), this);
-  }
+    const buttonRetour = this.create('button');
+    this.add(buttonRetour);
+    buttonRetour.innerHTML = "Menu Principal";
+    buttonRetour.onclick = () => {
+      this.controleur.changerVue(new VueMenuPrincipal(this.controleur), this);
+    }
 }

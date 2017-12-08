d622140daea88e818c39b20cd6a3f4ac82ca9c1c
MARTIN Jeremy <Jeremy090218>
2017-12-08 14:09:25 +0100

    message

--- www/js/vue/VueOptions.js
+++ www/js/vue/VueOptions.js
@@ -1,5 +1,12 @@
 class VueOptions extends Vue {
   constructor(ctrl) {
       super(ctrl, 'Options');
+
+      const buttonRetour = this.create('button');
+      this.add(buttonRetour);
+      buttonRetour.innerHTML = "Menu Principal";
+      buttonRetour.onclick = () => {
+        this.controleur.changerVue(new VueMenuPrincipal(this.controleur), this);
+      }
   }
 }

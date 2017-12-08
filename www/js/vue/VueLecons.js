bac617d701af1c805dcb02cf35cbb7f5836c84ef
MARTIN Jeremy <Jeremy090218>
2017-12-08 16:30:24 +0100

    JeremyM_LeconsV1

--- www/js/vue/VueLecons.js
+++ www/js/vue/VueLecons.js
@@ -8,5 +8,24 @@ class VueLecons extends Vue {
     buttonRetour.onclick = () => {
       this.controleur.changerVue(new VueMenuPrincipal(this.controleur), this);
     }
+
+    // Création du vecteur contenant les leçons
+    let cours = [];
+
+    // Ajout des leçons dans le vecteur
+    cours = ["Angles", "Thales", "Equations", "Espace", "Fonctions_num", "Fonctions_lin", "Identites", "Nombres_entiers", "Probabilites", "Puissances", "Racines", "Sphere", "Statistiques", "Trigo"];
+
+    // Création d'un bouton pour chaque cours
+
+    for (let lecon of cours) {
+      const button = this.create('button');
+      this.add(button);
+      button.innerHTML = lecon;
+      button.onclick = () => {
+        this.controleur.changerVue(new VueLecon(this.controleur, lecon), this);
+      }
+    }
+
 }
+
 }

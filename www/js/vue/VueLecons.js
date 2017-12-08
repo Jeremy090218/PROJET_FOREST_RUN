5a20b8676d0ac1266fde9da9e1af4400581d8ae5
MARTIN Jeremy <Jeremy090218>
2017-12-08 16:45:09 +0100

    JeremyM_LeconsV2

--- www/js/vue/VueLecons.js
+++ www/js/vue/VueLecons.js
@@ -13,7 +13,7 @@ class VueLecons extends Vue {
     let cours = [];
 
     // Ajout des leçons dans le vecteur
-    cours = ["Angles", "Thales", "Equations", "Espace", "Fonctions_num", "Fonctions_lin", "Identites", "Nombres_entiers", "Probabilites", "Puissances", "Racines", "Sphere", "Statistiques", "Trigo"];
+    cours = ["Angles Inscrits & Polygones Reguliers", "Thales", "Equations, Inequations & Systemes", "Geometrie dans l'Espace", "Fonction Numerique", "Fonctions Lineaires & Affines", "Identites Remarquables & Equations Produit", "Nombres Entiers & Rationnels", "Probabilites", "Puissances d'Exposants Relatifs", "Racines Carrees", "Sphere", "Statistiques", "Trigonometrie"];
 
     // Création d'un bouton pour chaque cours
 

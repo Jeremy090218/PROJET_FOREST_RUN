d0620e841830e403f7f53996e0d9222f53e4156e
BENETTI Mathilde <benettim@hostname.iut2.upmf-grenoble.fr>
2018-01-10 17:39:10 +0100

    Police

--- www/js/vue/VuePerdu.js
+++ www/js/vue/VuePerdu.js
@@ -2,34 +2,40 @@ class VuePerdu extends Vue {
   constructor(ctrl) {
     super(ctrl, 'perdu');
 
+    const divB = this.create('div');
+    this.add(divB);
+    divB.id = "divB";
+
+    const buttonPerdu = this.create('button');
+    this.add(divB;buttonPerdu);
+    buttonPerdu.innerHTML = "Menu Principal";
+    buttonPerdu.onclick = () => {
+        this.controleur.changerVueUnique(new VueMenuPrincipal(this.controleur));
+    }
+
     const titre = this.create('h1');
-    this.add(titre);
+    this.add(divB,titre);
     titre.innerHTML = "PERDU";
 
     this.message = this.create('h2');
-    this.add(this.message);
+    this.add(divB,this.message);
     this.message.innerHTML = "Vous avez obtenu";
 
     this.score = this.create('p');
-    this.add(this.score);
+    this.add(divB,this.score);
     this.score.innerHTML = this.controleur.partieRunner.getScore()+" points";
 
     this.pieces = this.create('p');
-    this.add(this.pieces);
+    this.add(divB,this.pieces);
     this.pieces.innerHTML = this.controleur.partieRunner.getPieceRecup();
     this.pieces.id = "nbPiecesVuePerdu";
 
     this.add(this.pieces, this.controleur.textures.getObjet("Coin_1.png"));
 
     this.pieces = this.create('p');
-    this.add(this.pieces);
+    this.add(divB,this.pieces);
     this.pieces.innerHTML = "Votre meilleur score est de "+this.controleur.getUtilisateur().getHighScore()+" points";
 
-    const buttonPerdu = this.create('button');
-    this.add(buttonPerdu);
-    buttonPerdu.innerHTML = "Menu Principal";
-    buttonPerdu.onclick = () => {
-        this.controleur.changerVueUnique(new VueMenuPrincipal(this.controleur));
-    }
+
   }
 }

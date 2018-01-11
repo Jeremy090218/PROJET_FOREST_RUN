13c6a9fa86a357cb35a2fa00fcf153997104ba4e
BENETTI Mathilde <benettim@hostname.iut2.upmf-grenoble.fr>
2018-01-11 12:11:27 +0100

    Univers + CSS

--- www/js/vue/VueUnivers.js
+++ www/js/vue/VueUnivers.js
@@ -14,9 +14,21 @@ class VueUnivers extends Vue {
     this.add(titre);
     titre.innerHTML = "Univers";
 
+    const divU = this.create('div');
+    this.add(divU);
+    divU.id = "divU";
+
     const blabla = this.create('p');
-    this.add(blabla);
-    blabla.innerHTML = "bfhgirifh hg^rei hgig erihg igi hoâ fqcçg euipfrê gôuagû iger^rui^gruiê";
+    this.add(divU, blabla);
+    blabla.innerHTML = "Bienvenu dans ForestRun, un monde où règne la paix depuis des millénaire. Un univers où cohabite différents animaux, prédateurs et proies vivent en parfaite harmonie.";
+
+    const blabla2 = this.create('p');
+    this.add(divU, blabla2);
+    blabla2.innerHTML = "Mais voilà que l'équilibre est menacée, les arbres Centenaires qui maintenaient cet équilibre ont été contaminés par une force obscurs et certains animaux ont commencé à s'en prendre aux autres.";
+
+    const blabla3 = this.create('p');
+    this.add(divU, blabla3);
+    blabla3.innerHTML = "Incarne un de ses animaux et rétablis l'ordre !";
 
   }
 }
